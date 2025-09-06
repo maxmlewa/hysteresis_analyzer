111c46fca418d0031b58658d022088eb64fef87b
Max Mlewa <maxmlewa26@gmail.com>
2025-09-06 01:43:13 -0400

    Model fitting header

---
+++ include/ModelFitter.h
@@ -0,0 +1,19 @@
+#ifndef MODEL_FITTER_H
+#define MODEL_FITTER_H
+
+#include "DataLoader.h"
+#include <vector>
+#include <array>
+
+class ModelFitter {
+public:
+    // Parameters: {a, b, c, d} for the model
+    static std::array<double, 4> fitModel(const std::vector<DataPoint>& data,
+                                            double Ms = 1.0);
+    
+    static double modelFunction(double H, double Ms, double a, double b, double c, double d);
+
+}
+
+
+#endif
